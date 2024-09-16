5ec05654b0e67e2da36f8c30a375a49357f5e3fa
Marina Guimarães <57577355+moliveira5@users.noreply.github.com>
2024-09-15 22:10:06 -0300

    Add files via upload

---
+++ OCESO/ObterContasUsuarios/ObterDoSistema.cs
@@ -0,0 +1,46 @@
+﻿using System.Management;
+
+namespace ObterContasUsuarios
+{
+    public static class ObterDoSistema
+    {
+        public static void listarUsuarios()
+        {
+            string query = "SELECT * FROM Win32_UserAccount WHERE LocalAccount = TRUE";
+            ManagementObjectSearcher resultado = new ManagementObjectSearcher(query);
+            ManagementObjectCollection usuarios = resultado.Get();
+
+            foreach (ManagementObject usuario in usuarios)
+            {
+                // Exibe o nome do usuário e o status da conta
+                string nomeUsuario = usuario["Name"].ToString();
+
+                string tipoUsuario = usuario["SIDType"].ToString();
+                switch (tipoUsuario)
+                {
+                    case "1":
+                        tipoUsuario = "Usuário Normal";
+                        break;
+                    case "2":
+                        tipoUsuario = "Grupo";
+                        break;
+                    case "3":
+                        tipoUsuario = "Domínio";
+                        break;
+                    case "5":
+                        tipoUsuario = "Conta Bem Conhecida (Administrador ou Convidado)";
+                        break;
+                    case "6":
+                        tipoUsuario = "Usuário Deletado";
+                        break;
+                    default:
+                        tipoUsuario = "Outro";
+                        break;
+                }
+
+                Console.WriteLine($"Usuário: {nomeUsuario} | Tipo: {tipoUsuario}");
+            }
+
+        }
+    }
+}
---
+++ OCESO/ObterContasUsuarios/ObterPorPasta.cs
@@ -0,0 +1,19 @@
+﻿namespace ObterContasUsuarios
+{
+    public static class ObterPorPasta
+    {
+        public static void listarUsuarios()
+        {
+            string diretorioUsers = @"C:\Users";
+
+            string[] pastaUsuarios = Directory.GetDirectories(diretorioUsers);
+
+            Console.WriteLine("Usuários da pasta:");
+            foreach (string pasta in pastaUsuarios)
+            {
+                string nomeUsuario = Path.GetFileName(pasta);
+                Console.WriteLine(nomeUsuario);
+            }
+        }
+    }
+}
